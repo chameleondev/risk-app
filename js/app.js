6f67e196d8bb9f6258b5049e2256a196cdf54ab0
dillon2013 <dillonlee@live.co.uk>
2014-07-29 00:21:21 +0100

    views added

--- js/app.js
+++ js/app.js
@@ -1,1 +1,30 @@
 var app = angular.module('risk-tool',['ngAnimate','ngRoute']);
+
+app.config(function($routeProvider){
+	$routeProvider
+	.when('/population',{
+		templateUrl : 'partials/population.html'
+	})
+	.when('/bars',{
+		templateUrl : 'partials/bars.html'
+	})
+	.when('/relative',{
+		templateUrl : 'partials/relative.html'
+	})
+})
+
+app.directive( 'goClick', function ( $location ) {
+  return function ( scope, element, attrs ) {
+    var path;
+
+    attrs.$observe( 'goClick', function (val) {
+      path = val;
+    });
+
+    element.bind( 'click', function () {
+      scope.$apply( function () {
+        $location.path( path );
+      });
+    });
+  };
+});
